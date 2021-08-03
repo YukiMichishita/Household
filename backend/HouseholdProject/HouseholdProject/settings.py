2d58feddb630f293e576117a95a992fa4a9a0ce8
YukiMichishita <michishita.ocu@gmail.com>
2021-08-03 21:06:03 +0900

    ログ設定

--- backend/HouseholdProject/HouseholdProject/settings.py
+++ backend/HouseholdProject/HouseholdProject/settings.py
@@ -160,6 +160,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
+
 # ログ出力先のディレクトリを設定する
 LOG_BASE_DIR = os.path.join("/var", "log", "app")
 LOGGING = {
